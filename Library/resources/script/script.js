ed3dbcacf232283c04694dd4427d27a35e4bc558
Chuan Chen (Shawn) <61473858+Chuan-Chen@users.noreply.github.com>
2022-08-03 11:47:42 -0400

    updates

--- Library/resources/script/script.js
+++ Library/resources/script/script.js
@@ -8,6 +8,17 @@ function Book(title, author, pages, read){
 }
 
 
-function addBookToLibrary(){
-    
+function addBookToLibrary(title, author, pages, read){
+    myLibrary.push(new Book(title, author, pages, read));
 }
+
+
+const addBtn = document.querySelector(".add").addEventListener("click", function(e){
+    addBookToLibrary("harry", "potter", 1000, "not read");
+});
+const removeBtn = document.querySelector(".remove").addEventListener("click", function(e){
+
+});
+const modifyBtn = document.querySelector(".modify").addEventListener("click", function(e){
+
+});

d8f818dd7ed1eabea8d93e34fa66723fdeeb25c2
Whykari <61473858+Chuan-Chen@users.noreply.github.com>
2022-08-05 18:22:49 -0400

    added functionality

--- Library/resources/script/script.js
+++ Library/resources/script/script.js
@@ -1,24 +1,64 @@
 let myLibrary = [];
+let removeMode = false;
+let modifyMode = false;
+const content = document.querySelector(".content");
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, bookElement){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.book = bookElement;
+    this.bookDescriptions = bookElement;
+}
+
+function addBookToLibrary(title, author, pages, read, book){
+    myLibrary.push(new Book(title, author, pages, read, book));
+}
+
+function createBook(e){
+    const book = document.createElement("div");
+    const bookDescriptions = [];
+    for(let i = 0; i < 4; i++){
+        const temp = document.createElement("div");
+        book.appendChild(temp);
+        bookDescriptions.push(temp);
+    }
+    book.classList.add("book");
+    book.addEventListener("click", function(e){
+        if(removeMode){
+            book.remove();
+            myLibrary.pop();
+        }
+    });
+    addBookToLibrary("", "", 0, "", book, bookDescriptions);
+    content.append(book);
 }
 
 
-function addBookToLibrary(title, author, pages, read){
-    myLibrary.push(new Book(title, author, pages, read));
-}
 
-
-const addBtn = document.querySelector(".add").addEventListener("click", function(e){
-    addBookToLibrary("harry", "potter", 1000, "not read");
-});
+const addBtn = document.querySelector(".add").addEventListener("click", createBook);
 const removeBtn = document.querySelector(".remove").addEventListener("click", function(e){
-
+    removeMode = !removeMode;
+    if(removeMode && !modifyMode){
+        this.classList.add("mode");
+    }else{
+        this.classList.remove("mode");
+    }
 });
 const modifyBtn = document.querySelector(".modify").addEventListener("click", function(e){
+    modifyMode = !modifyMode;
+    if(!removeMode && modifyMode){
+        this.classList.add("mode");
+    }else{
+        this.classList.remove("mode");
+    }
+});
 
+window.document.addEventListener("keydown", (e) => {
+    if(e.keyCode == 32){ 
+        myLibrary[myLibrary.length-1].book.remove();
+        myLibrary.pop();
+    }
+    
 });

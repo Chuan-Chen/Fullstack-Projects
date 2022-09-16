d89bfa8f5c0ea4b836ab1a9f78cfda7960aad85a
Whykari <61473858+Chuan-Chen@users.noreply.github.com>
2022-09-16 18:05:21 -0400

    updated

--- IP/index.js
+++ IP/index.js
@@ -1,9 +1,19 @@
 
 const ipDiv = document.querySelector("#ip");
+const countryDiv = document.querySelector("#country");
 
+
+async function getAddress(ip){
+    let response =  await fetch(`http://ip-api.com/json/${ip}`).then(res => res.json()).then((data)=> {
+        countryDiv.textContent = data.country + ", " + data.city + " " + data.regionName;
+    });
+};
 
 async function getIP(url){
-    let response =  await fetch(url).then(res => res.json()).then((data) => ipDiv.textContent = data.ip);
+    let response =  await fetch(url).then(res => res.json()).then((data) => {
+        getAddress(data.ip);
+        ipDiv.textContent = data.ip;
+    });
     return response;
 }
 

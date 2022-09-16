237d9506f50c0e205303f2440bd321076c3efbf3
Whykari <61473858+Chuan-Chen@users.noreply.github.com>
2022-09-16 19:05:46 -0400

    finished IP

--- IP/index.js
+++ IP/index.js
@@ -1,12 +1,22 @@
 
 const ipDiv = document.querySelector("#ip");
 const countryDiv = document.querySelector("#country");
+const coordDIv = document.querySelector("#coord");
+const container = document.querySelector(".containerBK");
 
+async function getWallpaper(){
+    let height = window.innerWidth;
+    let response = await fetch(`https://picsum.photos/${height}`).then(res => {
+        document.body.style.backgroundImage = `url(${res.url})`;
+    });
+}
 
 async function getAddress(ip){
     let apiURL = `https://ipapi.co/${ip}/json/`;
     let response =  await fetch(apiURL).then(res => res.json()).then((data)=> {
         countryDiv.textContent = data.country_name + ", " + data.city + " " + data.region;
+        coordDIv.textContent = `latitude: ${data.latitude}, longitude: ${data.longitude}`
+        getWallpaper();
     });
 };
 

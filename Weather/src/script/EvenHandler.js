7517dbe3511d812a3aaebadd5ebc925ba4f84998
Chuan Chen (Shawn) <61473858+Chuan-Chen@users.noreply.github.com>
2022-09-19 10:33:09 -0400

    updated

--- Weather/src/script/EvenHandler.js
+++ Weather/src/script/EvenHandler.js
@@ -23,17 +23,20 @@ function conversion(domObjects){
 async function search(domObjects){
     domObjects.search.addEventListener('keydown', async (e)=>{
         if(e.keyCode == 13 && domObjects.search.value != ""){
+            
             let address = await getAddress("","","","","",domObjects.search.value);
+            //console.log(address)
             let coords = address.features[0].geometry.coordinates;
-            let weather = await getWeather(domObjects.unit, {lat: coords[0], lon: coords[1], time: new Date()});
-            console.log(weather)
-            createCard(domObjects);
+            //console.log(coords);
+            let weather = await getWeather(domObjects.unit, {lat: coords[1], lon: coords[0], time: new Date()});
+            //console.log(weather)
+            createCard(domObjects, weather);
         }
     });
 }
 
 
-export function createCard(domObjects){
+export function createCard(domObjects, weather){
     let content = domObjects.content;
     let cardContent = document.createElement("div");
     let cardContentTitle = document.createElement("div");
@@ -42,12 +45,22 @@ export function createCard(domObjects){
     let cardWindDirection = document.createElement("div");
     let cardWindSpeed = document.createElement("div");
     
+    console.log(weather.currentweather)
+/*
     cardContentTitle.textContent = domObjects.search.value;
     domObjects.search.value = "";
     cardUnit.textContent = domObjects.unit.textContent;
+    cardTemp.textContent = weather.currentWeather.temperature;
+    cardWindDirection.textContent = weather.currentWeather.winddirection;
+    cardWindSpeed.textContent = weather.currentWeather.windspeed;
 
+    */
     cardContent.appendChild(cardContentTitle);
     cardContent.appendChild(cardUnit);
+    cardContent.appendChild(cardTemp);
+    cardContent.appendChild(cardWindDirection);
+    cardContent.appendChild(cardWindSpeed);
+
 
     cardContent.classList.add("card");
     content.appendChild(cardContent);

e513a00f5b302a21e60cc30f03e817c933031f51
Chuan-Chen <Chuan.Chen402@student.qcc.cuny.edu>
2023-06-04 10:16:24 -0400

    fix_calc

--- Calculator/resources/script/script.js
+++ Calculator/resources/script/script.js
@@ -137,7 +137,9 @@ function keyPress(e){
     //console.log("current: " + currentOperations)
 }
 function buttonClick(e){
-    let key = e.path[0].attributes[1].value;
+    console.log(e.target.dataset.text)
+    //e.path[0].attributes[1].value;
+    let key = e.target.dataset.text;
     if(key == "="){
         display.textContent = "";
         currentOperations = reduce(currentOperations);
